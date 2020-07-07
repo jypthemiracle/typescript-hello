270a8831230421e535cb32c57b76b8a6cac7e9f1
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-07-07 11:48:05 +0900

    typescript tslint setting

---
+++ dist/tutorial/hello.js
@@ -0,0 +1,9 @@
+"use strict";
+var str = "hello world!";
+var Coffee = /** @class */ (function () {
+    function Coffee(name, temperature) {
+        this.name = name;
+        this.temperature = temperature;
+    }
+    return Coffee;
+}());
---
+++ tutorial/hello.ts
@@ -0,0 +1,11 @@
+const str: string = "hello world!";
+
+class Coffee {
+  private name: String;
+  private temperature: String;
+
+  constructor(name: String, temperature: String) {
+    this.name = name;
+    this.temperature = temperature;
+  }
+}
